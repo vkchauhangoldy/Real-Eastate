bba93334b1d54cbf02e5f8dd10ce98ec93430f77
sanchitha kp <sanchitha.2209@gmail.com>
2023-03-02 11:45:21 +0530

    added

--- frontend/src/Components/MainPage/SearchPage.js
+++ frontend/src/Components/MainPage/SearchPage.js
@@ -1,17 +1,26 @@
 import React from "react"
-import { useState } from "react"
+import { useState ,useEffect} from "react"
 import { HiSearch } from "react-icons/hi";
 import "./Alldetails.css"
 
 // import "./Search.css"
 const Search=()=>{
     const[search,setSearch]=useState("")
+    const[searchData,setSearchData]=useState([])
+    useEffect(()=>{
+        fetch(`http://localhost:3000/h1/key`).then((res)=>{
+           return res.json()
+        }).then((data)=>{
+            console.log(data)
+        // res.send(data)
+    })
+    },[])
     const searchhandle=()=>{
         // if(search){
         //     if(search){
-        //         let result= fetch(``)
+        //         let result= fetch(`http://localhost:3000/h1/key${search}`)
         //         result= result.json()
-        //         getData(result)
+        //         searchData(result)
         //         console.log(result)
         //     }
     
